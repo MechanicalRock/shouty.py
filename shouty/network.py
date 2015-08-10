d934ef104dd0ae4876f81bd11d54c1b101d74873
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-08-10 14:40:43 +0100

    Create a Network. Move person lookup method there

---
+++ shouty/network.py
@@ -0,0 +1,13 @@
+from shouty.person import Person
+
+class Network:
+    def __init__(self):
+        self.people = {}
+
+    def find_or_create_person(self, person_name):
+        if (person_name not in self.people):
+            self.people[person_name] = Person(self)
+        return self.people[person_name]
+
+    def broadcast(self, message):
+        pass
