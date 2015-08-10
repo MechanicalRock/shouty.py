39f48136bbacfacec4ddc4eb9c72fa9c3c7a83c5
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-08-10 16:42:47 +0100

    Unit tests for broadcast in/out of range

--- shouty/network.py
+++ shouty/network.py
@@ -1,4 +1,5 @@
 from shouty.person import Person
+from haversine import haversine
 
 class Network:
     def __init__(self):
@@ -9,6 +10,10 @@ class Network:
             self.people[person_name] = Person(self)
         return self.people[person_name]
 
-    def broadcast(self, message):
+    def broadcast(self, message, shout_geo_location):
         for listener in self.people.itervalues():
-            listener.hear(message)
+            if (self._in_range(shout_geo_location, listener.geo_location)):
+                listener.hear(message)
+
+    def _in_range(self, loc1, loc2):
+        return haversine(loc1, loc2) <= 1.0 # 1km = 1000m
--- test/network_test.py
+++ test/network_test.py
@@ -1,10 +1,24 @@
 from shouty.network import Network
 from mock import Mock
 
-def test_network_broadcast_delivers_message_to_all_people():
+leicester_square =  [51.5114242, -0.1287315]
+piccadilly_circus = [51.5101210, -0.1341683]
+heathrow =          [51.4715066, -0.4879044]
+
+def test_network_broadcast_delivers_message_to_all_people_in_range():
     network = Network()
     lucy = Mock()
+    lucy.geo_location = piccadilly_circus
     network.people = {'Lucy': lucy}
 
-    network.broadcast("hello")
+    network.broadcast("hello", leicester_square) # Leicester Square Station
     lucy.hear.assert_called_once_with("hello")
+
+def test_network_broadcast_does_not_deliver_message_to_people_outside_range():
+    network = Network()
+    lucy = Mock()
+    lucy.geo_location = piccadilly_circus
+    network.people = {'Lucy': lucy}
+
+    network.broadcast("hello", heathrow) # Leicester Square Station
+    assert not lucy.hear.called
